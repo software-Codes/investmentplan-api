c3159e29277529e61acdc52ab71883761f33b135
CollinsMunene-Developer <collinsentrepreneur@gmail.com>
2025-04-16 06:27:06 +0000

    feat: Add smile-identity-core integration for authentication

---
+++ src/authentication/src/Config/smile-id.js
@@ -0,0 +1,5 @@
+const smileIndetityCore = require('smile-identity-core');
+const WebApi = smileIdentityCore.WebApi;
+const IDApi = smileIdentityCore.IDApi;
+const Signature = smileIdentityCore.Signature;
+const Utilities = smileIdentityCore.Utilities;
