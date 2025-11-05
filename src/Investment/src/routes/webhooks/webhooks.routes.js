fe8d50e5e09b6f5b8087d2c98c65ba87a0550941
cm-collins <dev-collins@outlook.com>
2025-11-05 08:59:42 +0000

    remove the bulk investment folder

--- src/Investment/src/routes/webhooks/webhooks.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const WebhookController = require('../../controllers/webhook/webhook.controller');
-
-router.post('/binance', WebhookController.handleWebhook);
-
-
-module.exports = router;
